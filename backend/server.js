697e205b4158a3eda8f2cd97ee5eb5554c07d7a2
Atiq Rathod <atiq.rathod27@gmail.com>
2025-04-09 16:48:13 +0400

    Database Setup Completed!

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch(err => console.error('❌ Connection error:', err));
   
   // ✅ Start the Server
   const port = process.env.PORT || 27017;
