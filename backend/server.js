933286530e159bb6eb0509dded215e1cee7fccaa
Atiq Rathod <atiq.rathod27@gmail.com>
2025-04-09 16:46:31 +0400

    Cors Setup

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,17 @@ const app = express();
 app.use(express.json());
 app.set("port", process.env.PORT || 27017);
 
-
+// Enable CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  next();
+});
   
   // ✅ Start the Server
   const port = process.env.PORT || 27017;
